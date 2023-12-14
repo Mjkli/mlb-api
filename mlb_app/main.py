6390cfea984e8eeb09bad2b2db9d8bbd4592fa71
anthony <anthony@anthonys-MacBook-Air.local>
2023-12-13 21:12:34 -0800

    starting retival of player data

--- mlb_app/main.py
+++ mlb_app/main.py
@@ -1,2 +1,10 @@
-print("hello")
+from pybaseball import  playerid_lookup
+from pybaseball import  statcast_pitcher
+
+id = playerid_lookup('ohtani', 'shohei')
+id = id.iloc[0]['key_mlbam']
+
+ohtani_stats = statcast_pitcher('2017-06-01', '2017-07-01', id)
+
+print(ohtani_stats)
 
